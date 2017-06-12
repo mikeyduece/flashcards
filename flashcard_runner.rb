0b0b0eafd129e872fab40d039b0e8780d5c2f75b
Mike Heft <mikeheft@gmail.com>
2017-06-12 11:44:19 -0600

    work on runner file

---
+++ flashcard_runner.rb
@@ -0,0 +1,12 @@
+require './lib/card'
+require './lib/guess'
+require './lib/deck'
+require './lib/round'
+
+card_1 = Card.new("What is 5 + 5?", "10")
+card_2 = Card.new("What is Rachel's favorite animal?","red panda")
+card_3 = Card.new("Who is Sal's favorite singer?","Taylor Swift")
+card_4 = Card.new("Who is Ali's favorite singer?","Justin Beiber")
+deck = Deck.new([card_1,card_2,card_3,card_4])
+
+puts "Welcome! You're playing with #{deck.count} cards"
