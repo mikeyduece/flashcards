994e9a422e3726f7caeba45cbd373869a34c8664
Mike Heft <mikeheft@gmail.com>
2017-06-11 20:39:51 -0600

    i3 done

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,12 @@
-class Deck 
+class Deck
 
+  attr_reader :cards
+
+  def initialize(cards=nil)
+    @cards = cards
+  end
+
+  def count
+    cards.count
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,7 +6,28 @@ class DeckTest < Minitest::Test
     deck = Deck.new
   end
 
-  def method_name
+  def test_it_can_make_a_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Deck, deck
+  end
 
+  def test_it_can_store_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Array, deck.cards
+    assert_instance_of Card, deck.cards[0]
+  end
+
+  def test_it_can_count_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal 3, deck.count
   end
 end
